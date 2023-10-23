07776de68afe1930ed8beb9ab7f13420636cb6f2
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-23 14:46:20 +0900

    Applied favicon.

--- x-com/src/app/layout.tsx
+++ x-com/src/app/layout.tsx
@@ -1,12 +1,14 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Favicon from '../../public/favicon.ico';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'X. 무슨 일이 일어나고 있나요? /X',
     description: 'X. 무슨 일이 일어나고 있나요? /X',
+    icons: [{ rel: 'icon', url: Favicon.src }],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
