0a3b471523eec7141f66f093f320dc21748ee97d
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-23 21:20:42 +0900

    Created postForm component in home.

--- x-com/src/app/(afterLogin)/home/page.tsx
+++ x-com/src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,16 @@
+import Post from './_component/Post';
+import PostForm from './_component/PostForm';
+import Tab from './_component/Tab';
+import TabProvider from './_component/TabProvider';
+import style from './home.module.css';
+
 export default function Page() {
-    return <div>Home</div>;
+    return (
+        <div className={style.main}>
+            <TabProvider>
+                <Tab />
+                <PostForm />
+            </TabProvider>
+        </div>
+    );
 }
