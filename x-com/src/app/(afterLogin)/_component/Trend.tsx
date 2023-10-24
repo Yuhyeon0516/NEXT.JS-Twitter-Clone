1034050c22570d429dad9c297500e6591d036f1a
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-24 21:14:39 +0900

    changed layout when the address is /explore.

--- x-com/src/app/(afterLogin)/_component/Trend.tsx
+++ x-com/src/app/(afterLogin)/_component/Trend.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
-import style from './trend.module.css';
+import Link from "next/link";
+import style from "./trend.module.css";
 
 export default function Trend() {
     return (
-        <Link href={`/seach?q=트렌드`} className={style.container}>
+        <Link href={`/search?q=트렌드`} className={style.container}>
             <div className={style.count}>실시간트렌드</div>
             <div className={style.title}>유현</div>
             <div className={style.count}>1,234 posts</div>
