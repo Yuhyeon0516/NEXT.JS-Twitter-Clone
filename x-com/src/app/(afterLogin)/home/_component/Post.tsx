3f8596bae6bcf1efb9e9b905fc46cecec9a46cf3
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-24 11:52:29 +0900

    Created post component.

---
+++ x-com/src/app/(afterLogin)/home/_component/Post.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import style from './post.module.css';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+import ActionButtons from './ActionButtons';
+
+dayjs.locale('ko');
+dayjs.extend(relativeTime);
+
+export default function Post() {
+    const target = {
+        User: {
+            id: 'elonmusk',
+            nickname: 'Elon Musk',
+            image: '/X_black.ico',
+        },
+        content: 'X 클론코딩 중',
+        createdAt: new Date(),
+        Images: [],
+    };
+    return (
+        <article className={style.post}>
+            <div className={style.postWrapper}>
+                <div className={style.postUserSection}>
+                    <Link href={`/${target.User.id}`} className={style.postUserImage}>
+                        <img src={target.User.image} alt={target.User.nickname} />
+                        <div className={style.postShade} />
+                    </Link>
+                </div>
+                <div className={style.postBody}>
+                    <div className={style.postMeta}>
+                        <Link href={`/${target.User.id}`}>
+                            <span className={style.postUserName}>{target.User.nickname}</span>
+                            &nbsp;
+                            <span className={style.postUserId}>@{target.User.id}</span>
+                            &nbsp; · &nbsp;
+                        </Link>
+                        <span className={style.postDate}>{dayjs(target.createdAt).fromNow(true)}전</span>
+                    </div>
+                    <div>{target.content}</div>
+                    <div className={style.postImageSection}>
+                        {/* {target.Images && target.Images.length > 0 && (
+                            <Link href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`} className={style.postImageSection}>
+                                <img src={target.Images[0]?.link} alt="" />
+                            </Link>
+                        )} */}
+                    </div>
+                    <ActionButtons />
+                </div>
+            </div>
+        </article>
+    );
+}
